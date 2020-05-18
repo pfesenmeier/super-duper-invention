4ad1f1153bca35c8910af2a9897bbcb4e7909fc4
Paul <pfesenmeier@gmail.com>
2020-05-18 18:03:01 -0400

    repo ignores buffers

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 'use strict';
 
+
+
 const Hapi = require('@hapi/hapi');
 
-const init = async () => {
-
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello World';
-    }
-
-  });
-
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
-};
-
-process.on('unhandledRejection', (err) => {
-
-  console.log(err);
-  process.exit(1);
-
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost'
+});
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request , h) => {
+  return 'Hello World!';
+ }
 });
 
-init();
+exports.init = async() => {
+
+  await server.initialize();
+  return server;
+};
+
+exports.start = async() => {
+
+  await server.start();
+  console.log(`server running at ${server.info.uri}`);
+  return server;
+}
+
+proccess.on('Unhandled Rejection', (err) => {
+  console.log(err);
+  proccess.exit(1);
+})
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
-
+const { init, stop } = require('../server');
 const { expect } = Code;
 const { after, before, describe, it } = exports.lab = Lab.script();
 
 describe('index route', () => {
+  let server;
+  before(server = await init());
 
-  before(() => {});
-
-  after(() => {} );
+  after(server.stop());
 
   it('returns true when 1 + 1 equals 2', () => {
       expect(1+1).to.equal(2);
